812512b1bbf520f320217799193282eabcf1a520
Danilka1604 <168182470+Danilka1604@users.noreply.github.com>
2024-10-27 17:42:00 +0300

    Update Задание 2.py

--- Задание 2.py
+++ Задание 2.py
@@ -1,14 +1,1 @@
-import pandas as pd
-import numpy as np
 
-df = pd.read_csv("train_2.csv")
-
-age_fare_array = df[['Age', 'Fare']].values
-age_fare_array = age_fare_array[~np.isnan(age_fare_array[:, 0])]
-sorted_age_fare_array = age_fare_array[np.argsort(age_fare_array[:, 0])]
-
-youngest_passenger = sorted_age_fare_array[0]
-oldest_passenger = sorted_age_fare_array[-1]
-
-print(f"Информация о самом молодом пассажире (возраст, стоимость билета): {youngest_passenger}")
-print(f"Информация о самом старом пассажире (возраст, стоимость билета): {oldest_passenger}")
