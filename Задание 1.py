ded7fc8a02fd13d13494ddd76162b74a60376827
Danilka1604 <168182470+Danilka1604@users.noreply.github.com>
2024-10-27 17:42:11 +0300

    Update Задание 1.py

--- Задание 1.py
+++ Задание 1.py
@@ -1,7 +1,1 @@
-import pandas as pd
-import numpy as np
 
-df = pd.read_csv('train_2.csv')
-survived_array = df['Survived'].values
-total_survived = np.sum(survived_array)
-print("Общее количество выживших пассажиров:", total_survived)
